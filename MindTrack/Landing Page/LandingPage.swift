2009c79cb89d49caf9c0c419e97d6b66cc991df5
Demir Dereli <demir.dereli@rapsodo.com>
2023-09-08 13:59:24 +0300

    Started working on the Landing Page

---
+++ MindTrack/Landing Page/LandingPage.swift
@@ -0,0 +1,20 @@
+//
+//  LandingPage.swift
+//  MindTrack
+//
+//  Created by Demir Dereli on 8.09.2023.
+//
+
+import SwiftUI
+
+struct LandingPage: View {
+    var body: some View {
+        Image("Logo")
+    }
+}
+
+struct LandingPage_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingPage()
+    }
+}
